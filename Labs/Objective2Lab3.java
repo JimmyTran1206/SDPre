5385463bed037fbca462349fbe420c76c877e654
JimmyTran <quantrannhu@gmail.com>
2023-10-22 14:53:10 -0700

    finish Objective2Lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,10 @@
+public class Objective2Lab3{
+	public static void main (String[] args){
+		int year = 1988;
+		String month = "June";
+		boolean isWeekend= false;
+		System.out.println(year);
+		System.out.println(month);
+		System.out.println(isWeekend);
+	}
+}
