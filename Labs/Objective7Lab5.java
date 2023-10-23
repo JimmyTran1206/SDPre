d2b5fbcab450f33ddf5170c32bf856b40d885858
JimmyTran <quantrannhu@gmail.com>
2023-10-23 14:33:06 -0700

    finish Objective7Lab5

---
+++ Labs/Objective7Lab5.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Objective7Lab5 {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int selection;
+
+    while (true) {
+      System.out.println("_____Menu_____");
+      System.out.println("1: Say Hello");
+      System.out.println("2: List My favorite foods");
+      System.out.println("3: Exit");
+      System.out.println();
+
+      selection = scanner.nextInt();
+      if(selection==1){
+        System.out.println("Hello human");
+      }else if (selection==2){
+        System.out.println("Apple, Bananas, Coconuts");
+      }else if (selection==3){
+        break;
+      }else{
+        System.out.println("Invalid selection. Please choose 1, 2, or 3.");
+      }
+
+    }
+
+    scanner.close();
+  }
+}
